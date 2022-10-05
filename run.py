3d14c94288a59c7c20f20b4f427479b9ae3d6493
TriggerhappyJ <jacob30wheeler@gmail.com>
2022-10-05 17:23:10 +1300

    Changed wording, added comments

--- run.py
+++ run.py
@@ -1,25 +1,25 @@
-from itertools import count
 from flask import *
-from credentials import *
 import tweepy
 
 app = Flask(__name__)
-'''
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
-auth.set_access_token(accessToken, accessTokenSecret)'''
 
 # Create API object
 api = tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAAGFJhwEAAAAAtmgQqi4yRSbewBgx1Kd7%2FbfvrEM%3DNmdOePO17SLwaJpBtpdYJUVh9rOUOjUhwy5NNJJjm4XcRbcu6T')
 
-def getHashStats(tag):
-    countData = api.get_recent_tweets_count(tag, granularity='day')
+
+# Uses user query to retrieve data from api
+def getSearchedStats(query):
+    # Retieves data from api
+    countData = api.get_recent_tweets_count(query, granularity='day')
+
+    # Grabs specific data wanted (todays count, weekly count)
     dayCount = countData.data[-1]['tweet_count']
     weekCount = countData.meta['total_tweet_count']
+
     return {'dayCount':str(dayCount), 'weekCount':str(weekCount)}
 
 @app.route('/')
 def index():
-    countData = getHashStats('lol')
+    countData = getSearchedStats('#lol')
     return("Day count: " + countData['dayCount'] + "</br> Week Count: " + countData['weekCount'])
     
